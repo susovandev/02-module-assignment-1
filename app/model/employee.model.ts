39852f858db6430d0fb73b866730cda996891ef8
Susovan Dev <susovandas.dev@gmail.com>
2025-11-18 09:57:31 +0530

    feat(employee-management): implement employee schema with types, model setup and DB indexing

---
+++ app/model/employee.model.ts
@@ -0,0 +1,44 @@
+import { Document, Schema, model } from 'mongoose';
+
+/*
+
+
+const employeeSchema = new mongoose.Schema({ name: { type: String, required: true },
+
+email: { type: String, required: true, unique: true }, department: { type: String, required: true }, position: { type: String },
+
+salary: { type: Number },
+
+hiredDate: { type: Date, default: Date.now },
+
+});
+*/
+export interface IEmployeeDocument extends Document {
+	name: string;
+	email: string;
+	department: string;
+	position: string;
+	salary: number;
+	hiredDate: Date;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const employeeSchema = new Schema<IEmployeeDocument>(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		department: { type: String, required: true },
+		position: { type: String },
+		salary: { type: Number },
+		hiredDate: { type: Date, default: Date.now },
+	},
+	{ timestamps: true },
+);
+
+employeeSchema.index({ email: 1 }, { unique: true });
+employeeSchema.index({ name: 1, department: 1 }, { unique: true });
+
+const employeeModel = model<IEmployeeDocument>('Employee', employeeSchema);
+
+export default employeeModel;
